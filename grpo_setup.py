89a1195b6f8da69374428a1d26734470412da739
Nhat Lam <150221983+lamnh404@users.noreply.github.com>
2026-04-02 21:04:32 +0700

    Update grpo_setup.py

--- grpo_setup.py
+++ grpo_setup.py
@@ -8,10 +8,10 @@ hf_token = secrets.get_secret("lalala")
 login(token=hf_token)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-WEIGHTS_DIR = os.path.join(BASE_DIR, "weights", "Qwen2.5-VL-7B-Instruct-GPTQ-Int4")
+WEIGHTS_DIR = os.path.join(BASE_DIR, "weights", "hfl/Qwen2.5-VL-7B-Instruct-GPTQ-Int4")
 
 snapshot_download(
-    repo_id="Qwen/Qwen2.5-VL-7B-Instruct-GPTQ-Int4",
+    repo_id="hfl/Qwen2.5-VL-7B-Instruct-GPTQ-Int4",
     local_dir=WEIGHTS_DIR,
     local_dir_use_symlinks=False,
     token=hf_token,
