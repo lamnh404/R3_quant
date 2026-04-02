12f6a34f35558ad37e607f4ff6bed4ed6ecd9fd3
Nhat Lam <150221983+lamnh404@users.noreply.github.com>
2026-04-02 20:47:25 +0700

    Authenticate Hugging Face with Kaggle Secrets token
    Added login functionality to authenticate with Hugging Face using a token from Kaggle Secrets.

--- grpo_setup.py
+++ grpo_setup.py
@@ -1,5 +1,11 @@
-from huggingface_hub import snapshot_download
+from huggingface_hub import snapshot_download, login
+from kaggle_secrets import UserSecretsClient
 import os
+
+# Lấy token từ Kaggle Secrets
+secrets = UserSecretsClient()
+hf_token = secrets.get_secret("lalala")
+login(token=hf_token)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 WEIGHTS_DIR = os.path.join(BASE_DIR, "weights", "Qwen2.5-VL-7B-Instruct-GPTQ-Int4")
@@ -7,5 +13,6 @@ WEIGHTS_DIR = os.path.join(BASE_DIR, "weights", "Qwen2.5-VL-7B-Instruct-GPTQ-Int
 snapshot_download(
     repo_id="Qwen/Qwen2.5-VL-7B-Instruct-GPTQ-Int4",
     local_dir=WEIGHTS_DIR,
-    local_dir_use_symlinks=False
+    local_dir_use_symlinks=False,
+    token=hf_token,
 )
